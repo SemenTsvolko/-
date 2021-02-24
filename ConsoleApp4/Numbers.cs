b269624fc79b97a6c69c74e8fb0650d86f9570ce
SemenTsvolko <75906654+SemenTsvolko@users.noreply.github.com>
2021-02-24 14:50:39 +0300

    Add files via upload

---
+++ ConsoleApp4/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Numbers
+    {
+        private int x;
+        private int y;
+        public Numbers(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            
+        }
+        public void BZ()
+        {
+            double r1 = Math.Pow(x, 3);
+            double r2 = Math.Pow(y, 3);
+            Console.WriteLine("Результат конечного X:"+ r1);
+            Console.WriteLine("Результат конечного Y:"+ r2);
+            double r3 = Math.Max(r1, r2);
+            Console.WriteLine("Куб большего из чисел:"+ r3);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число x:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число y:");
+            int y = int.Parse(Console.ReadLine());
+            Numbers numbers = new Numbers(x, y);
+            numbers.BZ();
+            
+            Console.ReadKey();
+        }
+    }
+}
